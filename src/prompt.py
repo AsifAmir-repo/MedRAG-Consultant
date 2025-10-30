5c2980854650eccdb0e144289d971de701025f36
Asif Amir Noor <asifcyberg@gmail.com>
2025-10-30 17:15:21 +0600

    Initial commit

---
+++ src/prompt.py
@@ -0,0 +1,10 @@
+# Defining Prompt
+prompt_template = """
+Use the following pieces of context to answer the user's question.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+Context: {context}
+Question: {question}
+
+Answer:
+"""
